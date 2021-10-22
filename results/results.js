c390d52be36b077eab100d3b168f59880662164c
Emma Egstad <emma@egstad.com>
2021-10-22 13:32:15 -0700

    added line graph to chart

--- results/results.js
+++ results/results.js
@@ -42,6 +42,10 @@ const captured = results.map((item) => {
     return item.timesCaught;
 });
 
+const encountered = results.map((item) => {
+    return item.timesShown;
+});
+
 var ctx = document.getElementById('resultsChart').getContext('2d');
 // eslint-disable-next-line no-undef
 new Chart(ctx, {
@@ -50,8 +54,39 @@ new Chart(ctx, {
         labels: names,
         datasets: [
             {
-                label: '# of Times Captured',
+                type: 'bar',
+                label: 'Times Captured',
                 data: captured,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                ],
+                borderWidth: 1,
+            },
+            {
+                type: 'line',
+                label: 'Times Encountered',
+                data: encountered,
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
